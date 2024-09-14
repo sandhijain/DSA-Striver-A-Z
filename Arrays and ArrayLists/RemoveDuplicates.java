d3c76cf6e93d5c6a513888a9b2d2df085ee34a97
SANDHI JAIN <sandhijain24.tundla@gmail.com>
2024-09-14 20:47:23 +0530

    New Questions solved

--- Arrays and ArrayLists/RemoveDuplicates.java
+++ Arrays and ArrayLists/RemoveDuplicates.java
@@ -1,5 +1,9 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class RemoveDuplicates {
-    public static int removeDuplicates(int[] nums) {
+    public static int removeDuplicatesOptimised1(int[] nums) {
     int n = nums.length;
    
     int uniqueCount = 1;
@@ -9,18 +13,46 @@ public class RemoveDuplicates {
             uniqueCount++;
         }
     }
+   
     return uniqueCount;
         
     }
     
     
-    public static int removeDuplicatesOptimised(int[] nums) {
-        return 0;
+    public static int removeDuplicatesOptimised2(int[] nums) {
+       int n = nums.length;
+       int j = 0;
+       for(int i=0;i<n;i++){
+        if(nums[j]!=nums[i]){
+            nums[j+1] = nums[i];
+            j++;
+        }
+       }
+       return j+1;
+    }
+
+    public static int removeDuplicatesBrute(int[] nums) {
+        Set<Integer>hs = new LinkedHashSet<>();
+        for(int num:nums){
+            hs.add(num);
+        }
+        int j=0;
+        for(int newNum: hs){
+            nums[j] = newNum;
+            j++;
+        }
+
+        return hs.size();
+        
     }
     public static void main(String[] args) {
         int[] nums = {-30, -30, 0, 0, 10, 20, 30, 30};
-        System.out.println( removeDuplicates(nums));
-       // System.out.println( removeDuplicatesOptimised(nums));
+        System.out.println( removeDuplicatesBrute(nums));
+        for(int num:nums){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+       
       
        
     }
